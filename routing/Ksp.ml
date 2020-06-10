8e0fc50a8db09e53a7d957751ca85c9d5bd38bfa
checomeon <czhang226-c@my.cityu.edu.hk>
2020-06-10 17:47:19 +0800

    add latest files of Shiwei

--- routing/Ksp.ml
+++ routing/Ksp.ml
@@ -1,26 +1,14 @@
 open Core
 
 open Apsp
+open Util
 open Yates_Types
-
-(***************)
-(* local state *)
-(***************)
-let prev_scheme = ref SrcDstMap.empty
-
-
-(***********************)
-(* algorithm interface *)
-(***********************)
 
 let name = "Ksp"
 
-(* Solve: Uniform distributoin over k-shortest paths *)
 let solve (topo:topology) (_:demands) budget: scheme =
-  let new_scheme =
-    if not (SrcDstMap.is_empty !prev_scheme) then !prev_scheme
-    else
-      let host_set = Util.get_hosts_set topo in
+  let raw_scheme =
+      let host_set = get_hosts_set topo in
       let all_ksp = all_pair_k_shortest_path topo budget host_set in
       SrcDstMap.fold all_ksp ~init:SrcDstMap.empty
         ~f:(fun ~key:(u, v) ~data:paths acc ->
@@ -29,8 +17,9 @@ let solve (topo:topology) (_:demands) budget: scheme =
               let path_map =
                 List.fold_left paths ~init:PathMap.empty
                   ~f:(fun acc path ->
-                      let prob = 1.0 /. Float.of_int (List.length paths) in
+                      (* let prob = 1.0 /. Float.of_int (List.length paths) in *)
+                      let prob = 1.0 /. Float.of_int (List.length path) in
                       PathMap.set acc ~key:path ~data:prob) in
               SrcDstMap.set acc ~key:(u, v) ~data:path_map) in
-  prev_scheme := new_scheme;
-  new_scheme
+  let normalized_scheme = normalize_scheme raw_scheme in
+  normalized_scheme
